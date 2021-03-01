c790b590c988723739ee6c6593b114dd2231ce70
Inderprit Singh <singhinderprit1992@gmail.com>
2021-02-28 19:55:05 -0500

    finished

---
+++ db/index.js
@@ -0,0 +1,98 @@
+const Sequelize = require("sequelize");
+const { DataTypes } = Sequelize;
+const db = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/naruto_db"
+);
+
+const Village = db.define("village", {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4,
+  },
+  name: {
+    type: DataTypes.STRING(15),
+  },
+});
+
+const Character = db.define("character", {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4,
+  },
+  name: {
+    type: DataTypes.STRING(15),
+  },
+});
+
+Character.belongsTo(Village, { as: "villager" });
+Village.hasMany(Character, { foreignKey: "villagerId", as: "members" });
+
+Character.belongsTo(Character, { as: "hokage" });
+Character.hasMany(Character, { foreignKey: "hokageId", as: "villagers" });
+
+const syncAndSeed = async () => {
+  await db.sync({ force: true });
+
+  const [
+    naruto,
+    hinata,
+    sasuke,
+    itachi,
+    raikage,
+    killerBee,
+    gaara,
+    kankuro,
+    hiddenLeaf,
+    hiddenCloud,
+    hiddenSand,
+  ] = await Promise.all([
+    Character.create({ name: "Naruto" }),
+    Character.create({ name: "Hinata" }),
+    Character.create({ name: "Sasuke" }),
+    Character.create({ name: "Itachi" }),
+    Character.create({ name: "Raikage" }),
+    Character.create({ name: "Killer Bee" }),
+    Character.create({ name: "Gaara" }),
+    Character.create({ name: "Kankuro" }),
+    Village.create({ name: "Hidden Leaf" }),
+    Village.create({ name: "Hidden Cloud" }),
+    Village.create({ name: "Hidden Sand" }),
+  ]);
+
+  naruto.villagerId = hiddenLeaf.id;
+  hinata.villagerId = hiddenLeaf.id;
+  sasuke.villagerId = hiddenLeaf.id;
+  itachi.villagerId = hiddenLeaf.id;
+  raikage.villagerId = hiddenCloud.id;
+  killerBee.villagerId = hiddenCloud.id;
+  gaara.villagerId = hiddenSand.id;
+  kankuro.villagerId = hiddenSand.id;
+
+  kankuro.hokageId = gaara.id;
+  hinata.hokageId = naruto.id;
+  sasuke.hokageId = naruto.id;
+  itachi.hokageId = naruto.id;
+  killerBee.hokageId = raikage.id;
+
+  await Promise.all([
+    naruto.save(),
+    hinata.save(),
+    sasuke.save(),
+    itachi.save(),
+    raikage.save(),
+    killerBee.save(),
+    gaara.save(),
+    kankuro.save(),
+  ]);
+};
+
+module.exports = {
+  db,
+  syncAndSeed,
+  models: {
+    Village,
+    Character,
+  },
+};
--- server.js
+++ server.js
@@ -1,76 +1,106 @@
-const { UUID } = require("sequelize");
-const Sequelize = require("sequelize");
-const { DataTypes } = Sequelize;
-const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost/naruto_db"
-);
+const {
+  db,
+  syncAndSeed,
+  models: { Village, Character },
+} = require("./db");
+const path = require("path");
 const express = require("express");
 const app = express();
 
-app.get("/api/villages", async (req, res, next) => {
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.get("/", async (req, res, next) => {
   try {
-    res.send(
-      await Village.findAll({
-        include: [
-          {
-            model: Character,
-            as: "members",
-          },
-        ],
-      })
-    );
+    const villages = await Village.findAll();
+    res.send(`
+    <html>
+      <head>
+        <link rel = 'stylesheet' href = '/public/styles.css'/>
+      </head>
+      <body>
+      <h1>Welcome to the Ninja World</h1>
+      <img src ="https://i.ytimg.com/vi/RsjJViqWz2Q/maxresdefault.jpg">
+      <img class ="imgTwo" src = "https://i.pinimg.com/originals/d5/40/ac/d540ac82faa864e7a94bc5c478fb7e59.gif"> 
+      <h2>Villages</h2>
+      <ul>
+      ${villages
+        .map(
+          (village) => `<a href = "/api/villages/${village.id}">
+        <li>${village.name}</li></a>`
+        )
+        .join("")}
+      </ul>
+      <a href = "/api/hokages" ><h2>Click Here To See The Hokages List</h2></a>
+      </body>
+    </html>`);
   } catch (ex) {
     next(ex);
   }
 });
 
-const Village = db.define("village", {
-  id: {
-    type: DataTypes.UUID,
-    primaryKey: true,
-    defaultValue: DataTypes.UUIDV4,
-  },
-  name: {
-    type: DataTypes.STRING(15),
-  },
+app.get("/api/hokages", async (req, res, next) => {
+  try {
+    const hokages = await Character.findAll({
+      where: {
+        hokageId: null,
+      },
+    });
+
+    res.send(`
+    <html>
+      <head>
+        <link rel = 'stylesheet' href = '/public/styles.css'/>
+      </head>
+      <body class = 'hokages'>
+      <h1>All Hail The Hokages</h1>
+      <h2 class = 'hokageUL'>Hokages</h2>
+      <ul class = 'hokageUL'>
+      ${hokages
+        .map((hokage) => `<li class ='hokageListItem'>${hokage.name}</li>`)
+        .join("")}
+      </ul>
+      </body>
+      <a href = "/"><h1 class = "homePage">CLICK HERE TO GO BACK TO HOME PAGE</h1></a>
+    </html>`);
+  } catch (ex) {
+    next(ex);
+  }
 });
 
-const Character = db.define("character", {
-  id: {
-    type: DataTypes.UUID,
-    primaryKey: true,
-    defaultValue: DataTypes.UUIDV4,
-  },
-  name: {
-    type: DataTypes.STRING(15),
-  },
+app.get("/api/villages/:id", async (req, res, next) => {
+  try {
+    const villages = await Village.findAll({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Character,
+          as: "members",
+        },
+      ],
+    });
+
+    res.send(`
+    <html>
+      <head>
+        <link rel = 'stylesheet' href = '/public/styles.css'/>
+      </head>
+      <body class = "villages">
+      <h1>VILLAGE MEMBERS</h1>
+      <h2>The Homies</h2>
+      <ul>
+      ${villages[0].members
+        .map((member) => `<li class = "homies">${member.name}</li>`)
+        .join("")}
+      </ul>
+      </body>
+      <a href = "/"><h1 class = "homePage">CLICK HERE TO GO BACK TO HOME PAGE</h1></a>
+    </html>`);
+  } catch (ex) {
+    next(ex);
+  }
 });
-
-Character.belongsTo(Village, { as: "villager" });
-Village.hasMany(Character, { foreignKey: "villagerId", as: "members" });
-
-const syncAndSeed = async () => {
-  await db.sync({ force: true });
-
-  const [
-    naruto,
-    zabuza,
-    gaara,
-    hiddenLeaf,
-    hiddenMist,
-    hiddenSand,
-  ] = await Promise.all([
-    Character.create({ name: "naruto" }),
-    Character.create({ name: "zabuza" }),
-    Character.create({ name: "gaara" }),
-    Village.create({ name: "hiddenLeaf" }),
-    Village.create({ name: "hiddenMist" }),
-    Village.create({ name: "hiddenSand" }),
-  ]);
-
-  naruto.villagerId = hiddenLeaf.id;
-  await naruto.save();
-};
 
 const init = async () => {
   try {
